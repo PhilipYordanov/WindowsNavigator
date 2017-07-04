ca8967a61b952f063ba4a9a775a2903bda2facdf
PhilipYordanov <fjordanov1@gmail.com>
2017-07-04 10:25:34 +0300

    -db.SaveChanges

--- SimpleWindowsNavigator/WindowsNavigator/WindowsNavigator/Program.cs
+++ SimpleWindowsNavigator/WindowsNavigator/WindowsNavigator/Program.cs
@@ -2,17 +2,20 @@
 {
     using System.Data.Entity;
     using System.Windows.Forms;
+    using WindowsNavigator.Data;
     using WindowsNavigator.Models;
 
     public static class Program
     {
         public static void Main()
         {
-            using (var dbContext = new _WindowsNavigator_Data_WindowsNavigatorDbContextDataSet())
+            using (var db = new WindowsNavigatorDbContext())
             {
                 if (!Database.Exists(@"Data Source=DESKTOP-52UDVT7\SQLEXPRESS;Initial Catalog=WindowsNavigator.Data.WindowsNavigatorDbContext;Integrated Security=True"))
                 {
-                    var test = new ButtonProp(1, "Test", "Test", "Test");
+                    var button = new ButtonProp(1, "Test", "Test", "Test");
+                    db.ButtonProp.Add(button);
+                    db.SaveChanges();
                 }
             }
 
